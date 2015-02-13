0c4ab961eb542d43b392b2fd49f5c77f5ed8b79c
Hector Leon <hectorleon.designer@gmail.com>
2015-02-13 10:42:32 +0100

    Grunt and NPM Packages Installed

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+  // 1. All configuration goes here 
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+
+    //Concatena
+    concat: {
+      dist: {
+        src: [
+            'src/js/*.js', // All JS in the libs folder
+        ],
+        dest: 'js/build/application.js',
+      } 
+    },
+
+    //Minifica
+    uglify: {
+      build: {
+        src: 'build/application.js',
+        dest: 'build/application.min.js'
+      }
+    }
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'build/css/style.css': 'src/css/style.scss'
+        }
+      } 
+    }
+
+    //Automate el save
+    watch: {
+      options: {
+        livereload: true,
+      },
+      css: {
+        files: ['src/css/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false,
+        }
+      },
+      scripts: {
+        files: ['src/js/*.js'],
+        tasks: ['concat', 'uglify'],
+        options: {
+          spawn: false,
+        },
+      } 
+    }
+
+
+  });
+
+  // 3. Where we tell Grunt we plan to use this plug-in.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+  grunt.registerTask('default', ['watch']);
+
+};
